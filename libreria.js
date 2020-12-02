17a7c3845eb5f8912ed915bbc8efb6a0374ca47a
miguel <usmo300@hotmail.com>
2020-12-02 12:18:24 +0100

    implementación de carpetas y añadir css
    se ha añadido la carpeta estática con su css, un ejemplo de contenido y cambio de app.engine

--- libreria.js
+++ libreria.js
@@ -2,7 +2,10 @@ const express =  require ('express');
 const exphbs = require ('express-handlebars');
 const app = express ();
 
-app.engine('handlebars', exphbs());
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({
+    layoutsDir: __dirname + '/views/layouts',
+}));
 app.set('view engine', 'handlebars');
 app.get('/',function(req, res){
 res.render('home');
