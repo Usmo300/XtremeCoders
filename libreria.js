29a12eb7c9f62b3604f69293a36d47992c92f5c3
Angel <litocrack@hotmail.com>
2020-12-09 13:42:09 +0100

    Cambiadas extensiones de handlebars a hbs

--- libreria.js
+++ libreria.js
@@ -3,10 +3,10 @@ const exphbs = require ('express-handlebars');
 const app = express ();
 
 app.use(express.static('public'));
-app.engine('handlebars', exphbs({
-    layoutsDir: __dirname + '/views/layouts',
+app.engine('hbs', exphbs({
+    layoutsDir: __dirname + '/views/layouts', extname: 'hbs'
 }));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 app.get('/',function(req, res){
 res.render('home');
 
