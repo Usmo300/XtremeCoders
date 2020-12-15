60544c6db5a359a9a3460658817c88930ab92b55
jcsedeno <sedenojc@gmail.com>
2020-12-15 10:07:32 +0100

    Aceptacion cambios libreria.js

--- libreria.js
+++ libreria.js
@@ -4,17 +4,12 @@ const app = express ();
 
 app.use(express.static('public'));
 app.engine('hbs', exphbs({
-<<<<<<< Updated upstream
  
-    layoutsDir: __dirname + '/views/layouts', 
-    partialsDir:__dirname + '/views/partials',
-        
+layoutsDir: __dirname + '/views/layouts', 
+partialsDir:__dirname + '/views/partials',
+       
+extname: 'hbs'
 
-    extname: 'hbs'
-
-=======
-    layoutsDir: __dirname + '/views/layouts',
->>>>>>> Stashed changes
 }));
 app.set('view engine', 'hbs');
 app.get('/',function(req, res){
