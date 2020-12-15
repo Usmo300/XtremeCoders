dca6903e5ade35b721374f622831ee057eb825e1
jcsedeno <sedenojc@gmail.com>
2020-12-15 09:56:22 +0100

    Creacion de HTM del login
    Creacion del login.hbs y cambio de nombre del  archivo nav.hbs por menuPrincipal.hbs

--- libreria.js
+++ libreria.js
@@ -4,9 +4,13 @@ const app = express ();
 
 app.use(express.static('public'));
 app.engine('hbs', exphbs({
+ 
     layoutsDir: __dirname + '/views/layouts', 
     partialsDir:__dirname + '/views/partials',
+        
+
     extname: 'hbs'
+
 }));
 app.set('view engine', 'hbs');
 app.get('/',function(req, res){
@@ -16,4 +20,3 @@ res.render('home');
 
 app.listen(3000);
 
-//hello world
